edb7c29a4e1cf55344ad02edc019b40a9dfd5c71
Dr-MonkyBrainF <wiiu.daisuki321@gmail.com>
2025-03-31 19:51:52 +0900

    color change commit

--- app/page.tsx
+++ app/page.tsx
@@ -6,22 +6,22 @@ import Image from 'next/image';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-[var(--bg-in)] p-4 md:h-52">
+    <main className="flex min-h-screen flex-col p-6 bg-[var(--bg-l4)]">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-[var(--bg-l3)] p-4 md:h-52">
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-indigo-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-[var(--bg-l3)] px-6 py-10 md:w-2/5 md:px-20">
+          <p className={`${lusitana.className} text-xl text-[var(--bg-l1)] md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-[var(--tx-col)]">
+            <a href="https://nextjs.org/learn/" className="text-green-100">
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
           </p>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-[var(--bg-in)] px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-indigo-800 md:text-base"
+            className="flex items-center gap-5 self-start rounded-lg bg-[var(--bg-l1)] px-6 py-3 text-sm font-medium text-white transition-colors hover:text-gray-200 hover:ring-2 hover:ring-[var(--bg-l2)] hover:bg-[var(--bg-l2)] md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,9 +1,14 @@
+'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
   FireIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -19,19 +24,26 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-[var(--bg-in)] hover:text-white md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+            "flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm text-white font-medium hover:bg-[var(--bg-l1)] hover:text-white md:flex-none md:justify-start md:p-2 md:px-3",
+            {
+              'bg-[var(--bg-l1)]': pathname === link.href,
+            },
+          )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
